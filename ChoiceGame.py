c4a6178333389238d7275380f5071bf237317e4f
Aadi <aadiamazing5@gmail.com>
2019-12-24 10:50:52 -0500

    added python files

---
+++ ChoiceGame.py
@@ -0,0 +1,79 @@
+print "Welcome to Jah Game, the game about Jah. Lets fucking get it."
+username = raw_input("What would you like your username to be? \n > ")
+
+print "So you want to be called %s? Well too bad, your name is now Jahseh Onfroy." % username
+
+print """ You wake up as the cool emo kid known as xxxtentacion. 
+You have released many bangers songs, such as Spotlight Moonlight Uh,
+SAD! and other depressing songs of that sort."""
+
+print """What would you like to do?
+1. Go back to sleep.
+2. Wake up and hop in your BMW i8 to whip to work.
+3. Kill yourself. """
+
+first_choice = raw_input("Type 1, 2 or 3 below. \n > ")
+
+if first_choice == "1":
+	print """Before going back to sleep, you decide to beat ur meat. Unfortunately,
+	you nut so hard you die."""
+	print "You died. Can we get an F in the chat? To restart Jah Game, press up and then enter."
+	
+elif first_choice == "2":
+	print """You get into your BMW i8, a very expensive sports car, and start
+	driving to Universal Studios, your place of work, with your Louis Vitton bag 
+	in the passenger seat."""
+	print """On the way there, you see a motorcycle shop that catches your eye.
+	What will you do?
+	1. Walk in.
+	2. Wait in your car."""
+	
+	second_choice = raw_input("> ")
+	
+	if second_choice == "1":
+		print """You walk into the store. Being the rich dude that you are,
+		you buy a couple ATVs and Harley Davidsons, then walk back to your car.
+		Just as you walk in, your crippling gonorrhoea hits you, and you die."""
+		print "You died. Can we get an F in the chat? To restart Jah Game, press up and then enter."
+		
+	elif second_choice == "2":
+		print """You decide to wait in your car, for whatever unknown reason. 
+		Suddenly, three niggas run up to your car with guns, eyeing your Louis V
+		bag. What will you do?
+		1. Try to fight them off.
+		2. Let them have the bag."""
+		
+		third_choice = raw_input("> ")
+		
+		if third_choice == "1":
+			print """You lunge at the niggas, who have now broken your car window 
+			and are reaching for your bag. You attempt to punch one of them, but forget
+			that they have a gun. They shoot you straight through the head."""
+			print "You died. Can we get an F in the chat? To restart Jah Game, press up and then enter."
+		
+		elif third_choice == "2":
+			print """You stay in your seat, letting them have the bag. You forget that they
+			are black, and so are violent as fuck. They shoot you straight through the 
+			head."""
+			print "You died. Can we get an F in the chat? To restart Jah Game, press up and then enter."
+		
+		else:
+			print "What the fuck does that mean?? Press 1 or 2 retard."
+
+	
+	else:
+		print "What the fuck does that mean?? Press 1 or 2 retard."
+	
+	
+	
+	
+	
+else: 
+	print """You take out your glock and shoot yourself. 
+	You die. What did you think was gonna happen?"""
+	print "You died. Can we get an F in the chat? To restart Jah Game, press up and then enter."
+	
+	
+	
+
+
